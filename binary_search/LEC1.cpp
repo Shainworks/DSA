dc4640c326edf7fb3b9d4da7f0650ace9724b728
Shainworks <shainworks033@gmail.com>
2025-09-08 19:43:28 +0530

    the findKthPositive problem has been updated

--- binary_search/LEC1.cpp
+++ binary_search/LEC1.cpp
@@ -548,5 +548,18 @@ bool search2(vector<int>& arr, int target) {    //To search for an element in a 
         }
         return low;
     }
+    int findKthPositive(vector<int>& arr, int k) {      //To find the kth missing positive number in a sorted array
+        //O(n) solution optimal solution
+        int n = arr.size();
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int missing = arr[mid] - (mid+1);
+            if(missing < k) low = mid+1;
+            else high = mid -1;
+        }
+        return (high+1+k);      //The kth missing number will be high+1+k
+    }
 int main(){
 }
