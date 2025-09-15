f7593e8e05228ab5e3d4e6b3fe8884e9a6a47ac7
Shainworks <shainworks033@gmail.com>
2025-09-15 19:54:09 +0530

    The splitArray problem as een uploaded

--- binary_search/LEC1.cpp
+++ binary_search/LEC1.cpp
@@ -561,5 +561,43 @@ bool search2(vector<int>& arr, int target) {    //To search for an element in a 
         }
         return (high+1+k);      //The kth missing number will be high+1+k
     }
+    int splitArray(vector<int>& nums, int k) {
+        return findPages(nums,nums.size(),k);
+    }
+    int countStudents(vector<int> &arr, int pages) {
+    int n = arr.size(); 
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            //add pages to current student
+            pagesStudent += arr[i];
+        }
+        else {
+            //add pages to next student
+            students++;
+            pagesStudent = arr[i];
+        }
+    }
+    return students;
+}
+int findPages(vector<int>& arr, int n, int m) {
+    //book allocation impossible:
+    if (m > n) return -1;
+
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int students = countStudents(arr, mid);
+        if (students > m) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
 int main(){
 }
