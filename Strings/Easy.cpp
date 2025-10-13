d30f43f245911abba98fa06656daad5a4125ff72
Shainworks <shainworks033@gmail.com>
2025-10-13 23:29:10 +0530

    largestOddNumber and reverseWords have been uploaded

--- Strings/Easy.cpp
+++ Strings/Easy.cpp
@@ -16,6 +16,42 @@ string removeOuterParentheses(string s) { //To remove the outermost parentheses 
         }
         return ans;
     }
+    string largestOddNumber(string num) {       //To find the largest odd number that can be formed by removing some digits from the given number
+        //O(n) solution 
+        int n = num.length() -1;
+        for(int i=n;i>=0;i--){
+            if((num[i]-'0')%2!=0){
+                return num.substr(0,i+1);
+            }
+        }
+        return "";
+    }
+    string reverseWords(string s) {     //To reverse the words in a given string
+        //O(n) solution
+        int left = 0;
+        int right = s.length()-1;
+        string temp = "";
+        string ans = "";
+        while(left<=right){
+            char ch = s[left];
+            if(ch != ' '){
+                temp += ch;
+            }
+            else {
+                if(ans!="") ans = temp +' '+ ans;
+                else ans = temp;
+                temp="";
+                while (left <= right && s[left] == ' ') left++;
+                left--;
+            }
+            left++;
+        }
+        if(temp!=""){
+            if(ans!="") ans = temp + ' ' + ans;
+            else ans = temp;
+        }
+        return ans;
+    }
 int main(){
    return 0;
 }
