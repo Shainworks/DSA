0662e3875eda63fbe5346705810d57a7aba432e6
Shainworks <shainworks033@gmail.com>
2025-10-16 23:53:52 +0530

    romanToInt problem has been uploaded

--- Strings/Easy.cpp
+++ Strings/Easy.cpp
@@ -128,6 +128,59 @@ string removeOuterParentheses(string s) { //To remove the outermost parentheses 
         }
         return Maxdepth;
     }
+    int romanToInt(string s) {      //To convert a roman numeral to an integer
+        //O(n) solution
+        int n = s.size();
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='I' && s[i+1]=='V'){
+                count = count +4;
+                i++;
+            }
+            else if(s[i]=='I' && s[i+1]=='X'){
+                count = count +9;
+                i++;
+            }
+            else if(s[i]=='I'){
+                count = count+1;
+            }
+            else if(s[i]=='V'){
+                count = count +5;
+            }
+            else if(s[i]=='X' && s[i+1]=='C'){
+                count = count +90;
+                i++;
+            }
+            else if(s[i]=='X' && s[i+1]=='L'){
+                count = count +40;
+                i++;
+            }
+            else if(s[i]=='X'){
+                count = count +10;
+            }
+            else if(s[i]=='L'){
+                count = count +50;
+            }
+            else if(s[i]=='C' && s[i+1]=='M'){
+                count = count +900;
+                i++;
+            }
+            else if(s[i]=='C' && s[i+1]=='D'){
+                count = count +400;
+                i++;
+            }
+            else if(s[i]=='C'){
+                count = count +100;
+            }
+            else if(s[i]=='D'){
+                count = count +500;
+            }
+            else if(s[i]=='M'){
+                count = count +1000;
+            }
+        }
+        return count;
+    }
 int main(){
    return 0;
 }
