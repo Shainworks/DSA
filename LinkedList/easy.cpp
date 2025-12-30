d0f5b5ceeafbea00d54e7f622dc1b12ec9853df0
Shainworks <shainworks033@gmail.com>
2025-12-30 19:50:38 +0530

    Reversing a dll

--- LinkedList/easy.cpp
+++ LinkedList/easy.cpp
@@ -12,5 +12,20 @@ void deleteNode(ListNode* node) {       //To delete a node in a singly linked li
         node->next = node->next->next;
         delete temp;
     }
+     Node *reverse(Node *head) {        //Reverse a doubly linked list using stack
+        Node *temp = head;
+        stack<int> st;
+        while(temp != NULL){
+            st.push(temp->data);
+            temp = temp->next;
+        }
+        temp = head;
+        while(temp != NULL){
+            temp -> data = st.top();        //Assigning values from stack to nodes
+            st.pop();       //Popping stack
+            temp = temp->next;  
+        }
+        return head;
+    }
 int main(){
 }
