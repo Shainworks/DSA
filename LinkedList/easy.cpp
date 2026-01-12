627a0d1a973600550adb273789cac9780c9a77bf
Shainworks <shainworks033@gmail.com>
2026-01-13 00:20:52 +0530

    Code to sort the Linkedlist has been uploaded

--- LinkedList/easy.cpp
+++ LinkedList/easy.cpp
@@ -5,6 +5,13 @@ using namespace std;
       ListNode *next;
       ListNode(int x) : val(x), next(NULL) {}
   };
+    struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
 void deleteNode(ListNode* node) {       //To delete a node in a singly linked list given only access to that node
         //O(1) solution
         node->val = node->next->val;
@@ -375,6 +382,96 @@ void deleteNode(ListNode* node) {       //To delete a node in a singly linked li
         slow->next = slow->next->next;      //Bypassing the middle node
         delete midnode;
         return head;
+    }
+    ListNode* sortList(ListNode* head) {        //Sort linked list using extra space
+        if(head == NULL ){
+            return NULL;
+        }
+        vector<int> arr;
+        ListNode *temp = head;
+        while(temp!=NULL){
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+        sort(arr.begin(),arr.end());
+        ListNode *curr = new ListNode(arr[0]);      //Creating new sorted linked list
+        ListNode *tempe = curr;
+        for(int i=1;i<arr.size();i++){
+            tempe->next = new ListNode(arr[i]);     //Adding nodes to sorted linked list
+            tempe = tempe->next;
+        }  
+        return curr;
+    }
+    ListNode* sortList(ListNode* head) {        //Sort linked list using merge sort (O(1) space)
+         // Base case: empty or single node
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        // Find middle node
+        ListNode* middle = findMiddle(head);
+
+        // Split into two halves
+        ListNode* right = middle->next;
+        middle->next = nullptr;
+        ListNode* left = head;
+
+        // Recursively sort both halves
+        left = sortList(left);
+        right = sortList(right);
+
+        // Merge sorted halves
+        return mergeTwoSortedLinkedLists(left, right);
+    }
+    ListNode* findMiddle(ListNode* head) {
+        // If list empty or single node
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        // Slow and fast pointers
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        // Move fast twice as fast as slow
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Return middle node
+        return slow;
+    }
+     ListNode* mergeTwoSortedLinkedLists(ListNode* list1, ListNode* list2) {
+        // Create a dummy node
+        ListNode* dummyNode = new ListNode(-1);
+        
+        // Temp pointer to build merged list
+        ListNode* temp = dummyNode;
+
+        // Traverse both lists
+        while (list1 != nullptr && list2 != nullptr) {
+            // Choose smaller node
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            // Move temp pointer
+            temp = temp->next;
+        }
+
+        // Attach remaining nodes
+        if (list1 != nullptr) {
+            temp->next = list1;
+        } else {
+            temp->next = list2;
+        }
+
+        // Return head of merged list
+        return dummyNode->next;
     }
 int main(){
 }
